1eb57f929902eed871e55b5ce9c743959ae98aef
Sharad Aghara <sharadaghara24@gmail.com>
2025-02-21 00:51:41 +0530

    solving exe-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arguments) {
+    let arr = arguments[0];
+    let removeElements = [];
+
+    for (let i = 1; i < arguments.length; i++) {
+        removeElements.push(arguments[i]);
+    }
+
+    for (let index = 0; index < removeElements.length; index++) {
+        let ind = arr.indexOf(removeElements[index]);
+
+        if (ind > -1) {
+            arr.splice(ind, 1);
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
