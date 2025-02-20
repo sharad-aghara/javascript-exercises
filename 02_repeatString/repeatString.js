f26e56e2b242d0dcc9b48792588da52fa5a7e525
Sharad Aghara <sharadaghara24@gmail.com>
2025-02-20 19:18:44 +0530

    [solving]: solved 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
 
+    let result = '';
+
+    if (times < 0) {
+        return 'ERROR';
+    }
+
+    for (let index = 0; index < times; index++) {
+        result += string;
+    }
+
+    // result = string.repeat(times);
+
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arr = [];
+    for (let index = 0; index < string.length; index++) {
+        arr.push(string[index]);        
+    }
 
+    return arr.reverse().join('');
 };
 
 // Do not edit below this line
